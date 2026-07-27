741a44fb6127798d4ab606712c5f606e951a1bbb
Sviatoslav000 <svyatyablonsky@gmail.com>
2026-07-27 17:44:03 +0000

    day6

--- day6/clean_app.py
+++ day6/clean_app.py
@@ -1,8 +1,9 @@
 import os
 import argparse
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("name")
 args = parser.parse_args()
 
-print(args.name)
+shutil.unpack_archive(args.name,'.')
---
+++ zippy.py
@@ -0,0 +1,4 @@
+import shutil
+
+# shutil.make_archive(base_name, format, root_dir)
+shutil.make_archive('day6/backup', 'zip', 'day4')
